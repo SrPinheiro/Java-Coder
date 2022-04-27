71a4152b847166de05b30baf1d04ee3607a98b9e
Leonardo Pinheiro <94575742+SrPinheiro@users.noreply.github.com>
2022-04-26 23:41:30 -0300

    Add files via upload

---
+++ Estudos/src/A1Fundamentos/Desafios/Desafio003.java
@@ -0,0 +1,20 @@
+package A1Fundamentos.Desafios;
+
+import java.util.Scanner;
+
+public class Desafio003 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] salario = new String[4];
+        for(byte c = 1; c < 4; c++){
+            System.out.printf("Digite o seu %dº salario:",c);
+            salario[c-1] = scan.nextLine().replace(",",".");
+        }
+        scan.close();
+        try{
+            System.out.printf("Media: %.2f", (Double.parseDouble(salario[0]) + Double.parseDouble(salario[1]) + Double.parseDouble(salario[2]))/3);
+        }catch (Exception e){
+            System.out.println("Digite apenas numeros!");
+        }
+    }
+}
---
+++ Estudos/src/A1Fundamentos/Desafios/Desafio006.java
@@ -0,0 +1,21 @@
+package A1Fundamentos.Desafios;
+
+
+import java.util.Scanner;
+
+public class Desafio006 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("digite o primeiro numero!");
+        var num1 = scan.nextLine().replace(",",".");
+        System.out.println("digite o segundo numero!");
+        var num2 = scan.nextLine().replace(",",".");
+        System.out.println("digite a opração desejada:");
+        System.out.println("(+) Adição  (-)subtração (*)multiplicação (/)Divisão (%)Resto");
+        var parametro = scan.nextLine();
+        System.out.print(parametro.equals("+") ? Float.parseFloat(num1) + Float.parseFloat(num2) : parametro.equals("-") ? Float.parseFloat(num1) - Float.parseFloat(num2) : "");
+        System.out.print(parametro.equals("*") ? Float.parseFloat(num1) * Float.parseFloat(num2) : parametro.equals("/") ? Float.parseFloat(num1) / Float.parseFloat(num2) : "");
+        System.out.print(parametro.equals("%") ? Float.parseFloat(num1) % Float.parseFloat(num2):"");
+
+    }
+}
