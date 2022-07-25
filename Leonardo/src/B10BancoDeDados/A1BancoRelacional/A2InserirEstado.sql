8f5203fd24629f5e284f7a64e1e1e3b0e9f696af
SrPinheiro <sr.pinheiro.ifnmg@gmail.com>
2022-07-24 22:24:18 -0300

    BancoDeDados

---
+++ Leonardo/src/B10BancoDeDados/A1BancoRelacional/A2InserirEstado.sql
@@ -0,0 +1,9 @@
+INSERT INTO estados (nome,sigla,regiao,populacao)
+values('acre', 'AC','NORTE' , 0.83);
+
+insert into estados
+    (nome,sigla,regiao,populacao)
+VALUES
+    ('Alagoas', 'AL', 'Nordeste', 3.38),
+    ('Amapa', "AP", 'Norte', 0.8),
+    ('Amazonas', 'AM', 'Norte', 4.06);
---
+++ Leonardo/src/B10BancoDeDados/A1BancoRelacional/A4ConsultarEstados.sql
@@ -0,0 +1,9 @@
+SELECT * FROM estados;
+
+SELECT * from estados 
+WHERE regiao = "Norte"
+WHERE POPULACAO > 5
+
+SELECT * FROM estados
+order by populacao desc;
+
